8536952bc05740dbed7acb5eda0ac93ac258607e
Dave Hudson <dave@applificationhq.com>
2018-01-10 17:06:24 +0000

    Removed unused Text component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import Text from './text/Text';
 import Content from './content/Content';
 import HeaderText from './headertext/HeaderText';
 import BodyText from './bodytext/BodyText';
 import Profile from './profile/Profile';
 import Divider from './divider/Divider';
 
-export { Text, Content, HeaderText, BodyText, Profile, Divider };
+export { Content, HeaderText, BodyText, Profile, Divider };
