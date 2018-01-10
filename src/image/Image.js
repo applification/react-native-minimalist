751eb50d77c299340195c272e77a3afb2e79ac3d
Dave Hudson <dave@applificationhq.com>
2018-01-10 19:55:07 +0000

    Image fullwidth component with tests

---
+++ src/image/Image.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Dimensions } from 'react-native';
+import PropTypes from 'prop-types';
+import AutoHeightImage from 'react-native-auto-height-image';
+
+class ImageElement extends Component {
+  static propTypes = {
+    imageURL: PropTypes.string.isRequired,
+  };
+
+  render() {
+    return (
+      <AutoHeightImage
+        width={Dimensions.get('window').width}
+        imageURL={this.props.imageURL}
+      />
+    );
+  }
+}
+
+export default ImageElement;
---
+++ src/image/Image.test.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Dimensions } from 'react-native';
+import renderer from 'react-test-renderer';
+
+import Image from './Image';
+
+describe('IMAGE', () => {
+  it('Renders image to full device width', () => {
+    const component = renderer
+      .create(
+        <Image
+          imageURL={
+            'https://scontent-lht6-1.cdninstagram.com/t51.2885-15/sh0.08/e35/p640x640/26152588_1188914984576015_4446380707253583872_n.jpg'
+          }
+        />
+      )
+      .toJSON();
+    const width = component.props.style[0].width;
+    const deviceWidth = Dimensions.get('window').width;
+
+    expect(width).toBe(deviceWidth);
+    expect(component).toMatchSnapshot();
+  });
+});
