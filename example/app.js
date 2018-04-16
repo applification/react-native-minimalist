ce53a8f4c1b4cb39d0ce4df193fa7606af9011fb
Dave Hudson <dave@applificationhq.com>
2018-04-16 21:20:19 +0100

    refactor(Removed storybook and expo example folder):

--- example/app.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import StorybookUI from './storybook';
-
-Expo.registerRootComponent(StorybookUI);
--- example/main.js
+++
@@ -1,6 +0,0 @@
-import Expo from 'expo';
-import StorybookUI from './storybook';
-
-import App from './app';
-
-module.exports = __DEV__ ? StorybookUI : App;
